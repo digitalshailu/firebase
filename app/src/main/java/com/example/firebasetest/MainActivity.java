f445e22c83bcd0f2c5b6a580918bcba05311b73f
shailendra <digitalshailu@gmail.com>
2018-08-17 20:40:08 +0530

    Firebase database with device id.

--- app/src/main/java/com/example/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.firebasetest;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,26 +10,32 @@ import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
+    private EditText editTextKey, editTextValue;
     private View button;
     Firebase mFirebase;
+
+    String deviceId = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Firebase.setAndroidContext(this);
-        mFirebase = new Firebase("https://fir-test-a5456.firebaseio.com");
+        //Get device id.
+        deviceId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
 
-        editText = findViewById(R.id.editText);
+        Firebase.setAndroidContext(this);
+        mFirebase = new Firebase("https://fir-test-a5456.firebaseio.com/Users/"+deviceId);
+
+        editTextKey = findViewById(R.id.editTextKey);
+        editTextValue = findViewById(R.id.editTextValue);
         button = findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Firebase childFireBase = mFirebase.child("key");
-                childFireBase.setValue(editText.getText().toString());
+                Firebase childFireBase = mFirebase.child(editTextKey.getText().toString());
+                childFireBase.setValue(editTextValue.getText().toString());
             }
         });
     }
